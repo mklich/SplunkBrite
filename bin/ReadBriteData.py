b88993eb0f9779518cef4acc8d7431d0dd537e2c
fklevmarken <nfkl@kth.se>
2012-10-11 11:26:39 -0700

    file change

--- bin/ReadBriteData.py
+++ bin/ReadBriteData.py
@@ -1,7 +1,7 @@
 import urllib2
 from xml.dom.minidom import parseString
 
-uri = "https://www.eventbrite.com/xml/event_search?app_key=2ZFPC3WOTA4UPERJKG&city=San+Francisco&date=This+month"
+uri = "https://www.eventbrite.com/xml/event_search?app_key=2ZFPC3WOTA4UPERJKG&country=US&max=100"
 
 def GetSearchData(uri):
 	searchResultsXml = urllib2.urlopen(uri)

3bd749374acfcc6741ca18f6a55557a83d45a83f
fklevmarken <nfkl@kth.se>
2012-10-11 11:41:08 -0700

    file change

--- bin/ReadBriteData.py
+++ bin/ReadBriteData.py
@@ -1,7 +1,8 @@
 import urllib2
 from xml.dom.minidom import parseString
+from datetime import datetime
 
-uri = "https://www.eventbrite.com/xml/event_search?app_key=2ZFPC3WOTA4UPERJKG&country=US&max=100"
+uri = "https://www.eventbrite.com/xml/event_search?app_key=2ZFPC3WOTA4UPERJKG&country=US&max=10"
 
 def GetSearchData(uri):
 	searchResultsXml = urllib2.urlopen(uri)
@@ -10,13 +11,14 @@ def GetSearchData(uri):
 	dom = parseString(xmlData)
 	output = ''
 	for x in dom.getElementsByTagName('event'):
-		 
-		a = x.getElementsByTagName('id')[0].firstChild.nodeValue
-		b = x.getElementsByTagName('title')[0].firstChild.nodeValue
-		c = x.getElementsByTagName('created')[0].firstChild.nodeValue
-		d = x.getElementsByTagName('latitude')[0].firstChild.nodeValue
-		e = x.getElementsByTagName('longitude')[0].firstChild.nodeValue
-		output = a+' '+b+' '+c+' '+d+' '+e
+
+		timestamp =  "datetime: " + str(datetime.now()) 
+		a = "id: " + x.getElementsByTagName('id')[0].firstChild.nodeValue
+		b = "title: " + x.getElementsByTagName('title')[0].firstChild.nodeValue
+		c = "created: " + x.getElementsByTagName('created')[0].firstChild.nodeValue
+		d = "latitude: " + x.getElementsByTagName('latitude')[0].firstChild.nodeValue
+		e = "longitude: " + x.getElementsByTagName('longitude')[0].firstChild.nodeValue
+		output =  timestamp+' '+a+' '+b+' '+c+' '+d+' '+e
 		print output	
 
 GetSearchData(uri)
